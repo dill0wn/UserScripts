8dbfd0331cd4d839b13709acc06f342afc7d5c64
Dillon Walls <dillon.walls@slashdotmedia.com>
2020-11-07 11:47:49 -0500

    version 1.0.0

--- sandbox.user.js
+++ sandbox.user.js
@@ -1,19 +1,33 @@
 // ==UserScript==
 // @name         Desktop Notification Tester
-// @namespace    http://example.tld
-// @version      0.9
 // @description  Testing Desktop notifications with tampermonkey
 // @author       dill0wn
+// @version      1.0.0
+// @updateUrl    https://github.com/dill0wn/UserScripts/raw/master/sandbox.user.js
+// @downloadUrl  https://github.com/dill0wn/UserScripts/raw/master/sandbox.user.js
+// @namespace    http://example.tld
 // @match        https://direct.playstation.com/*
 // @grant        GM_notification
 // ==/UserScript==
 
+/*
+    README:
+    - In TM Setttings, set Script Update -> Check Interval to 'Every 6 Hours'
+*/
+
+/*
+    @updateUrl Notes:
+    - You don't need @updateUrl, if installed by clicking a *.user.js link
+    - @updateUrl + @downloadUrl are not necessary, but can provide canonical style behavior, esp if copy/paste installed
+    - TM doesn't check immediately for updates
+    - github.com can cache updates for a few minutes.
+*/
 
 function notifyMe() {
 
     GM_notification({
         title:'Delayed Notification',
-        text:'Hey there! You\'ve been notified! (Version 9)',
+        text: `Hey there! You\'ve been notified! (Version ${GM_info.script.version})`,
         silent:false,
         onclick: function() {
             window.focus();
@@ -27,34 +41,3 @@ setTimeout(notifyMe, 2000);
 // setTimeout(function() {
 //     location.reload();
 // }, 4000);
-
-/*
-document.addEventListener('DOMContentLoaded', function() {
- if (!Notification) {
-  alert('Desktop notifications not available in your browser. Try Chromium.');
-  return;
- }
-
- if (Notification.permission !== 'granted') {
-  Notification.requestPermission();
- }
-});
-
-
-function notifyMe() {
- if (Notification.permission !== 'granted') {
-  Notification.requestPermission();
- }
- else {
-  var notification = new Notification('Notification title', {
-   icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-   body: 'Hey there! You\'ve been notified!',
-  });
-  notification.onclick = function() {
-   window.open('http://stackoverflow.com/a/13328397/1269037');
-  };
- }
-}
-
-notifyMe();
-*/
